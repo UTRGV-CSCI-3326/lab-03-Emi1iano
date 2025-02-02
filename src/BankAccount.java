d367ab9dce3987c6f98887c9b273b45d19e07a6e
Emi1iano <150746114+Emi1iano@users.noreply.github.com>
2025-02-02 00:45:51 +0000

    BankAccount

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
-
+public class BankAccount {
+    public static void main(String[] args) {
+        double cash = 2175.37;
+        cash += -302.50;
+        cash += 50.03;
+        cash /= 2;
+        cash += 20.00;
+        cash += -1;
+        cash *= 2;
+        cash += 1;
+        System.out.printf("Bobs amount in bank is: $" + "%.2f",cash);
+        System.out.println("");
+    }
+}
